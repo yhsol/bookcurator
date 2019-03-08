17472f139d044479efc278a1af5eed69fdd59324
yhsol <yhsol1592@gmail.con>
2019-03-08 21:32:28 +0900

    Updates

---
+++ precache-manifest.a3651af4c68303cd6b2e61083feab205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f31a6ce5aef7d8fdcbe4f1a9618a2a95",
+    "url": "/bookcurator/static/media/noPoster.f31a6ce5.jpg"
+  },
+  {
+    "revision": "67693896998d2a43cb6f",
+    "url": "/bookcurator/static/js/runtime~main.67693896.js"
+  },
+  {
+    "revision": "6301fc5b62ba88259aab",
+    "url": "/bookcurator/static/js/main.6301fc5b.chunk.js"
+  },
+  {
+    "revision": "16a55059d8d8f87c40db",
+    "url": "/bookcurator/static/js/2.16a55059.chunk.js"
+  },
+  {
+    "revision": "eea64f2f2dd04694b1611afd7253ef5b",
+    "url": "/bookcurator/index.html"
+  }
+];
