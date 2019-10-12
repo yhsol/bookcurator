ae6f7686d9ebbfeaac6bdc26f6427ed008228902
yhsol <yhsol1592@gmail.com>
2019-10-12 14:59:53 +0900

    add simple

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,75 +1,77 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
 
 const Header = styled.header`
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 64px;
-	z-index: 10;
-	box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.1);
-	background-color: white;
-	opacity: 0.98;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 64px;
+  z-index: 10;
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.1);
+  background-color: white;
+  opacity: 0.98;
 `;
 
 const HeaderArea = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	width: 75vw;
-	margin: 0 auto;
-	padding: 0 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 75vw;
+  margin: 0 auto;
+  padding: 0 10px;
 `;
 
 const HeaderTitle = styled.div`
-	height: 64px;
-	color: #ff8906;
-	font-size: 30px;
-	font-weight: 500;
-	text-align: center;
+  height: 64px;
+  color: #ff8906;
+  font-size: 30px;
+  font-weight: 500;
+  text-align: center;
 `;
 
-const HeaderNav = styled.ul`display: flex;`;
+const HeaderNav = styled.ul`
+  display: flex;
+`;
 
 const NavItem = styled.li`
-	padding: 0 14px;
-	height: 64px;
-	text-align: center;
-	color: ${(props) => (props.current ? '#ff8906' : 'rgba(0, 0, 0, 0.5)')};
-	&:hover {
-		color: #ff8906;
-	}
-	transition: color .05s linear;
-	font-size: 19px;
-	font-weight: 400;
+  padding: 0 14px;
+  height: 64px;
+  text-align: center;
+  color: ${props => (props.current ? "#ff8906" : "rgba(0, 0, 0, 0.5)")};
+  &:hover {
+    color: #ff8906;
+  }
+  transition: color 0.05s linear;
+  font-size: 19px;
+  font-weight: 400;
 `;
 
 const SLInk = styled(Link)`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 64px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 64px;
 `;
 
 export default withRouter(({ location: { pathname } }) => (
-	<Header>
-		<HeaderArea>
-			<HeaderTitle>
-				<SLInk to="/">Bookcurator</SLInk>
-			</HeaderTitle>
-			<HeaderNav>
-				<NavItem current={pathname === '/count'}>
-					<SLInk to="/count">Count</SLInk>
-				</NavItem>
-				<NavItem current={pathname === '/recent'}>
-					<SLInk to="/recent">Recent</SLInk>
-				</NavItem>
-				<NavItem current={pathname === '/search'}>
-					<SLInk to="/search">Search</SLInk>
-				</NavItem>
-			</HeaderNav>
-		</HeaderArea>
-	</Header>
+  <Header>
+    <HeaderArea>
+      <HeaderTitle>
+        <SLInk to="/">Bookcurator</SLInk>
+      </HeaderTitle>
+      <HeaderNav>
+        <NavItem current={pathname === "/count"}>
+          <SLInk to="/count">Count</SLInk>
+        </NavItem>
+        <NavItem current={pathname === "/recent"}>
+          <SLInk to="/recent">Recent</SLInk>
+        </NavItem>
+        <NavItem current={pathname === "/search"}>
+          <SLInk to="/search">Search</SLInk>
+        </NavItem>
+      </HeaderNav>
+    </HeaderArea>
+  </Header>
 ));
--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,44 +1,8 @@
-import React from 'react';
-import DetailPresenter from './DetailPresenter';
-import { bookApi } from '../../api';
+import React from "react";
 
-export default class extends React.Component {
-	constructor(props) {
-		super(props);
-		const { location: { pathname } } = props;
-		this.state = {
-			result: null,
-			error: null,
-			loading: true,
-			isDetail: pathname.includes('/detail/')
-		};
-	}
+const DetailContainer = () => {
+  console.log(this.props);
+  return <div />;
+};
 
-	// 위에서 constructor 메서드를 쓴 이유는 componentDidMount 메서드를 사용했기 때문인가? 
-
-	async componentDidMount() {
-		const { match: { params: { id } }, history: { push } } = this.props;
-		const { isDetail } = this.state;
-		const parsedID = parseInt(id);
-		if (isNaN(parsedID)) {
-			return push('/');
-		}
-		let result = null;
-		try {
-			if(isDetail) {
-				({data: result} = await bookApi.detail(parsedID));
-			}
-			console.log(result);
-		} catch {
-			this.setState({ error: "Cat't find infomation" })
-		} finally {
-			this.setState({ loading: false, result })
-		}
-	}
-
-	// render things to DetailPresenter
-	render() {
-		const { result, error, loading } = this.state;
-		return <DetailPresenter result={result} error={error} loading={loading} />;
-	}
-}
+export default DetailContainer;
