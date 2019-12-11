2713ac1dbed8d92161a8f86b1b44506a308abf0c
yhsol <yhsol1592@gmail.com>
2019-12-11 21:32:37 +0900

    add detail logic

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { movieApi, tvApi } from "../Api";
+import Section from "../Components/Section";
 import DetailPresenter from "./DetailPresenter";
 
-const DetailContainer = () => {
-  console.log(this.props);
-  return <DetailPresenter />;
+const Detail = props => {
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const {
+    match: {
+      params: { id }
+    },
+    location: { pathname }
+  } = props;
+  const parsedID = parseInt(id);
+  const isMovie = pathname.includes("/movie/");
+
+  const FetchData = async () => {
+    try {
+      if (isMovie) {
+        const { data: result } = await movieApi.movieDetail(parsedID);
+        setResult(result);
+        console.log(result);
+      } else {
+        const { data: result } = await tvApi.tvDetail(parsedID);
+        setResult(result);
+        console.log(result);
+      }
+    } catch (error) {
+      setError("Can't find infomation");
+    } finally {
+      setLoading(false);
+      console.log(result);
+    }
+  };
+
+  useEffect(() => {
+    FetchData();
+  }, []);
+
+  console.log(result);
+
+  return <DetailPresenter loading={loading} error={error} result={result} />;
 };
 
-export default DetailContainer;
+export default Detail;
